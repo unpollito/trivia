2e34274f3848f49326b06b3f58d2d3e157022778
David Martín-González <3705952+unpollito@users.noreply.github.com>
2020-11-21 22:43:57 +0000

    Implement logout

--- frontend/src/views/HomeView/HomeView.tsx
+++ frontend/src/views/HomeView/HomeView.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./HomeView.module.css";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import Button from "../../components/Button/Button";
+import { setLoginToken } from "../../helpers/loginData";
 
 function HomeView() {
+  const [hasLoggedOut, setHasLoggedOut] = useState(false);
+
+  const logout = () => {
+    setLoginToken(null);
+    setHasLoggedOut(true);
+  };
+
+  if (hasLoggedOut) {
+    return <Redirect to="/login" />;
+  }
+
   return (
     <div className="s-view">
+      <div onClick={logout} className={styles.logout}>
+        Logout
+      </div>
       <h1 className={styles.title}>Trivia</h1>
       <Link className={styles.link} to="/quiz">
         <Button
